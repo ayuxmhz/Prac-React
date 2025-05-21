a285bff06f72409d784a132026f31ee33ead664e
ayuxmhz <158758264+ayuxmhz@users.noreply.github.com>
2025-05-21 10:21:06 +0545

    error code

---
+++ Desktop/React/Learnreact/src/components/Card.jsx
@@ -0,0 +1,9 @@
+  export default function Card(){
+    return(
+      <div className="info">
+        <img src="" alt="" />
+        <h2>Do anything</h2>
+        <p>Make it</p>
+      </div>
+    )
+  }
---
+++ Desktop/React/Learnreact/src/doit/Card.jsx
@@ -0,0 +1,7 @@
+import Card from "./components/Card.jsx "  
+function Card(){
+  return(
+<Card/>
+ )
+}
+export default Card
